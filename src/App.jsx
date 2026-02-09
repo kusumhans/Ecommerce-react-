932abfee816fa838e99d398114532190446321b0
kusumhans <hanswork007@gmail.com>
2026-02-09 17:32:25 +0530

    Adding navbar component using reactstrap https://reactstrap.github.io/?path=/docs/components-navbar--navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import { Button } from 'reactstrap';
-import {Alert} from 'reactstrap';
-
+import Header from './component/Navbar/Header'
 function App() {
 
   return (
     <>
-    <h1>shopCart</h1>
-    <Button color="primary">Click Me</Button>
-    <Button color="danger">danger </Button>
-    <Alert color="primary">Hey! Pay attention.</Alert>
+    <Header expand="md" container="md" fixed='top'/>
+   
     </>
   )
 } 
---
+++ src/component/Navbar/Header.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Header.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+function Header(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...props} className='navbar'>
+        <NavbarBrand href="/" className="Header">shop Cart</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>  
+            <UncontrolledDropdown nav inNavbar style={{marginRight: '2rem'}}>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>cart</DropdownItem>
+                <DropdownItem>Settings</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Logout</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+              <NavbarText>Hi User</NavbarText>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
