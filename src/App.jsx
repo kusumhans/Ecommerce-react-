3f24f34138ff7ec9bb495d5e53f74a08b9662908
kusumhans <hanswork007@gmail.com>
2026-02-09 14:05:22 +0530

    Configured react strap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import { Button } from 'reactstrap';
+import {Alert} from 'reactstrap';
 
 function App() {
 
   return (
     <>
-     shopCart
+    <h1>shopCart</h1>
+    <Button color="primary">Click Me</Button>
+    <Button color="danger">danger </Button>
+    <Alert color="primary">Hey! Pay attention.</Alert>
     </>
   )
-}
+} 
 
 export default App
